ea5a322e64153b92f52e63bcb16fccd08bb757a6
Manuel Daniel Gutierrez Marin <manuelgutierrezmarin@Computer.local>
2025-01-14 17:53:41 -0500

    Adding user and admin list with disconnected style for future use

---
+++ src/components/UserList.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      {/* Render list */}
+    </div>
+  );
+}
+
+export default UserList;
